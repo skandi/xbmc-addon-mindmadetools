29067c8d405f51bf6d19c6152e6ec03ac5bc2064
Andreas Wetzel <andi@mindmade.org>
2012-10-23 00:22:49 +0200

    new persistent data storage facility
    + sayHi to mindmade function

--- lib/mindmade.py
+++ lib/mindmade.py
@@ -4,20 +4,25 @@ Created on Jul 18, 2011
 @author: andi
 '''
 
-__all__ = [ "log", "notify", "htmldecode", "fetchHttp" ]
+__all__ = [ "log", "notify", "htmldecode", "fetchHttp",
+            "getUID", "getPersistent", "setPersistent", "sayHi"]
 
-import os, re
+import os, re, time
+import threading
 import urllib, urllib2, HTMLParser
 import datetime
+import uuid
+
 import xbmc, xbmcaddon
+import simplejson as json
 
 addon = xbmcaddon.Addon()
-LOGFILE = os.path.join( addon.getAddonInfo('path'), "log.txt");
+LOGFILE = os.path.join( addon.getAddonInfo('path'), "resources", "log.txt");
+DATAFILE = os.path.join( addon.getAddonInfo('path'), "resources", "data.json");
 
 entitydict = { "E4": u"\xE4", "F6": u"\xF6", "FC": u"\xFC",
                "C4": u"\xE4", "D6": u"\xF6", "DC": u"\xDC",
                "2013": u"\u2013"}
-
 
 def log( msg):
     msg = msg.encode( "latin-1")
@@ -43,7 +48,8 @@ def htmldecode( s):
     return s
 
 def fetchHttp( url, args={}, hdrs={}, post=False):
-    log("fetchHttp(%s): %s" % ("POST" if post else "GET", url))
+    log( "fetchHttp(%s): %s" % ("POST" if post else "GET", url))
+    if args: log( "args: %s" % args)
     hdrs["User-Agent"] = "Mozilla/5.0 (X11; Linux i686; rv:5.0) Gecko/20100101 Firefox/5.0"
     if post:
         req = urllib2.Request( url, urllib.urlencode( args), hdrs)
@@ -60,3 +66,52 @@ def fetchHttp( url, args={}, hdrs={}, post=False):
     response.close()
 
     return responsetext
+
+
+def getPersistent( key, default = None):
+    if not os.path.exists( DATAFILE): return default
+    data = json.load( file( DATAFILE, "r"))
+    if key in data.keys():
+        return data[key]
+    return default
+
+def setPersistent( key, value):
+    if os.path.exists( DATAFILE): 
+        data = json.load( file( DATAFILE, "r"))
+    else:
+        data = dict();
+    
+    data[key] = value
+    json.dump( data, file( DATAFILE, "w"))
+    
+def getUID():
+    uid = getPersistent( "uid")
+    if not uid:
+        uid = str( uuid.uuid1())
+    setPersistent( "uid", uid)
+    return uid
+
+def _sendhome( data):
+    from jsonrpc import ServiceProxy
+    homeurl = "http://www.mindmade.org/~andi/research/python/server.py"
+    proxy = ServiceProxy( homeurl)
+    result = proxy.log( data)
+    log( "result %s" % result)
+
+def sayHi():
+    HI_PERIOD = 60 * 60 * 6     # actions within 6 hours are treated as one session
+    lastusage = getPersistent( "lastusage", 0)
+    now = int( time.time())
+    if now - lastusage > HI_PERIOD:
+        usagecount = getPersistent( "usagecount", 0)
+        usagecount += 1
+        uid = getUID()
+        addonid = addon.getAddonInfo( "id")
+        version = addon.getAddonInfo( "version")
+        logparams = { "userid": uid, "addon": addonid, "version": version, "usagecount": usagecount}
+        t = threading.Thread( target=_sendhome, args=(logparams,))
+        t.setDaemon( True)
+        t.start()
+        
+        setPersistent( "lastusage", now)
+        setPersistent( "usagecount", usagecount)
